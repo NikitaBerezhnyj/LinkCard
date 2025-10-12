7ce5d659bc7b0948356a1961141f17ec46361494
Nikita Berezhnyj <97048105+NikitaBerezhnyj@users.noreply.github.com>
2025-10-12 12:54:22 +0300

    refactor: removed official pages and link to them in footer

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -1,14 +1,9 @@
-import Link from "next/link";
 import styles from "@/styles/components/Footer.module.scss";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <div className={styles.links}>
-          <Link href="/terms">Умови користування</Link>
-          <Link href="/privacy">Політика конфіденційності</Link>
-        </div>
         <p>© {new Date().getFullYear()} LinkCard. Усі права захищено.</p>
       </div>
     </footer>
