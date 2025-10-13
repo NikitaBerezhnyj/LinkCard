e6aa653848ababeaef5fd660dac1e9f99eddf309
Nikita Berezhnyj <97048105+NikitaBerezhnyj@users.noreply.github.com>
2025-10-13 17:39:20 +0300

    feat: added auth middleware to upload files routes

--- server/src/routes/uploadRoutes.ts
+++ server/src/routes/uploadRoutes.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import { uploadAvatar, uploadBackground } from "../controllers/uploadController";
 import { uploadMiddleware } from "../middleware/uploadMiddleware";
+import { authMiddleware } from "../middleware/authMiddleware";
 
 const router = Router();
 
-router.post("/upload-avatar", uploadMiddleware.single("file"), uploadAvatar);
-router.post("/upload-background", uploadMiddleware.single("file"), uploadBackground);
+router.post("/upload-avatar", authMiddleware, uploadMiddleware.single("file"), uploadAvatar);
+
+router.post(
+  "/upload-background",
+  authMiddleware,
+  uploadMiddleware.single("file"),
+  uploadBackground
+);
 
 export default router;
