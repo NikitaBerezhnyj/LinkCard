0616092fb6da3c07118a5c61da937c58a20fb7d9
NikitaBerezhnyj <nikita.berezhnyj@gmail.com>
2026-09-01 11:24:51 +0300

    moved nodejs code to server-old/ and initialized asp.net core in server/

--- server/src/utils/jwtHelper.ts
+++
@@ -1,5 +0,0 @@
-import jwt from "jsonwebtoken";
-
-export const generateAuthToken = (userId: string): string => {
-  return jwt.sign({ userId }, process.env.JWT_PRIVATE_TOKEN!, { expiresIn: "7d" });
-};
