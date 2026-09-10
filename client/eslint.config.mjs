ceb15eae9bba6f165e7bf8db627207ff2c8564d2
NikitaBerezhnyj <nikita.berezhnyj@gmail.com>
2026-09-10 19:03:08 +0300

    chore: add client development configs

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,18 +1,37 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import sonarjs from "eslint-plugin-sonarjs";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+
+  {
+    plugins: {
+      sonarjs
+    },
+    rules: {
+      ...sonarjs.configs.recommended.rules,
+
+      "no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          args: "after-used",
+          ignoreRestSiblings: true
+        }
+      ],
+
+      "no-console": "off",
+
+      "sonarjs/no-commented-code": "off",
+      "sonarjs/pseudo-random": "off",
+      "sonarjs/cognitive-complexity": ["warn", 15]
+    }
+  },
+
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"])
 ]);
 
 export default eslintConfig;
